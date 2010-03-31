e07101bb03aea83ecd62cfa8bfaf53890fbd0be1
Patrick Reynolds <cpreynolds@gmail.com>
2010-03-31 10:56:23 -0400

    This morning I fixed some whitespace and added some additional comments. We've now moved to a 2-space tab convention. This makes the code more readable and will make using external editors much less hellacious. Finally, I ran eclipse's nice organize imports to import only what we need.

--- src/com/taboozle/taboozle.java
+++ src/com/taboozle/taboozle.java
@@ -1,27 +1,37 @@
 package com.taboozle;
 
-import com.taboozle.common.Card;
-
 import android.app.ListActivity;
 import android.os.Bundle;
-import android.widget.*;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
 
+import com.taboozle.common.Card;
+
+/**
+ * This is the activity class that kicks off Taboozle
+ * @author The Taboozle Team
+ */
 public class taboozle extends ListActivity {
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        Card card = new Card("perfidious",5);
-        String[] badWords = {"trust","betray","gossip","liar","backstab"};
-        card.AddBadWords(badWords);
-        TextView v = new TextView(this);
-        v.setText(card.GetName());
-        v.setTextSize(50);
-        v.setGravity(android.view.Gravity.CENTER);
-        this.getListView().addHeaderView(v);
-        this.getListView().setHeaderDividersEnabled(true);
-        this.setListAdapter(new ArrayAdapter<String>(this,
-           android.R.layout.simple_list_item_1,card.GetBadWords()));
-        this.getListView().setClickable(false);
-    }
+
+  /**
+   * onCreate - initializes the activity to display the word you have to cause
+   * your team mates to say with the words you cannot say below.
+   */
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    Card card = new Card("perfidious",5);
+    String[] badWords = {"trust","betray","gossip","liar","backstab"};
+    card.AddBadWords(badWords);
+    TextView v = new TextView(this);
+    v.setText(card.GetName());
+    v.setTextSize(50);
+    v.setGravity(android.view.Gravity.CENTER);
+    this.getListView().addHeaderView(v);
+    this.getListView().setHeaderDividersEnabled(true);
+    this.setListAdapter(new ArrayAdapter<String>(this,
+        android.R.layout.simple_list_item_1,card.GetBadWords()));
+    this.getListView().setClickable(false);
+  }
+
 }
