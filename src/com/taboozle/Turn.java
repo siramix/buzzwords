4b176eb8c66bc7440d6511bf1c06312cd0473f44
Patrick Reynolds <cpreynolds@gmail.com>
2010-05-27 18:06:09 -0400

    I added documentation and cleaned Turn.
    I moved some functions/members around and tried to document things more fully.

--- src/com/taboozle/Turn.java
+++ src/com/taboozle/Turn.java
@@ -30,19 +30,30 @@ import android.widget.ViewFlipper;
 public class Turn extends Activity
 {
 
+  /**
+   * This is a reference to the current game manager
+   */
   private GameManager curGameManager;
   
   /**
    * Boolean to track which views are currently active
    */
-  protected boolean AIsActive;
+  private boolean AIsActive;
   
   /**
    * Sound pool for playing the buzz sound on a loop.
    */
-  protected SoundPool soundPool;
-  protected int buzzSoundId;
-  protected int buzzStreamId;
+  private SoundPool soundPool;
+  
+  /**
+   * id of the buzz within android's sound-pool framework
+   */
+  private int buzzSoundId;
+  
+  /**
+   * id of the buzz's stream within android's sound-pool framework 
+   */
+  private int buzzStreamId;
   
   /**
    * Unique IDs for Options menu
@@ -52,7 +63,7 @@ public class Turn extends Activity
   protected static final int MENU_RULES = 2;  
  
   /**
-   *  Creates the menu items 
+   *  Creates the menu items for the options menu 
    */
   @Override
   public boolean onCreateOptionsMenu(Menu menu) 
@@ -65,7 +76,8 @@ public class Turn extends Activity
   } 
   
   /**
-   * OnClickListener for the buzzer button
+   * Listener for the buzzer that plays on touch-down and stops playing on
+   * touch-up.
    */
   private OnTouchListener BuzzListener = new OnTouchListener() 
   {
@@ -76,30 +88,87 @@ public class Turn extends Activity
         float streamVolumeCurrent = mgr.getStreamVolume(AudioManager.STREAM_MUSIC);
         float streamVolumeMax = mgr.getStreamMaxVolume(AudioManager.STREAM_MUSIC); 
         float volume = streamVolumeCurrent / streamVolumeMax;
-        boolean ret = false;
-        if( event.getAction() == MotionEvent.ACTION_DOWN )
+        boolean ret;
+        switch( event.getAction() )
         {
-          buzzStreamId = soundPool.play( buzzSoundId, volume, volume, 1, -1, 1.0f );
-          ret = true;
+          case MotionEvent.ACTION_DOWN:
+            buzzStreamId = soundPool.play( buzzSoundId, volume, volume, 1, -1, 1.0f );
+            ret = true;
+            break;
+          case MotionEvent.ACTION_UP:
+            soundPool.stop( buzzStreamId );
+            ret = true;
+            break;
+          default:
+            ret = false;              
         }
-        else if( event.getAction() == MotionEvent.ACTION_UP)
-        {
-          soundPool.stop( buzzStreamId );
-          ret = true;
-        }
-        else
-        {
-          ret = false;
-        }
+
         return ret;
-      }
-  };
+      } 
+      
+  }; // End BuzzListener
   
   /**
-   * constant for the logging tag
+   * Listener for the 'Correct' button. It deals with the flip to the next 
+   * card.
    */
-  //private static final String TAG = "taboozle";
-
+  private OnClickListener CorrectListener = new OnClickListener() 
+  {
+      public void onClick(View v) 
+      {
+        AIsActive = !AIsActive;
+        ViewFlipper flipper = (ViewFlipper) findViewById( R.id.ViewFlipper0 );
+        flipper.showNext();
+        curGameManager.ProcessCard( 0 );
+        ShowCard();
+      }  
+  }; // End CorrectListener
+  
+  /**
+   * Listener for the 'Skip' button. This deals with moving to the next card
+   * via the ViewFlipper, but denotes that the card was skipped;
+   */
+  private OnClickListener SkipListener = new OnClickListener() 
+  {
+      public void onClick(View v) 
+      {
+        AIsActive = !AIsActive;
+        ViewFlipper flipper = (ViewFlipper) findViewById( R.id.ViewFlipper0 );
+        flipper.showNext();
+        curGameManager.ProcessCard( 2 );
+        ShowCard();
+      }
+  }; // End SkipListener
+ 
+  /**
+   * CountdownTimer - This initializes a timer during every turn that runs a
+   * method when it completes as well as during update intervals.
+  */ 
+  private class TurnTimer extends CountDownTimer
+  {
+    
+    public TurnTimer(long millisInFuture, long countDownInterval) 
+    {
+      super(millisInFuture, countDownInterval);
+    }
+    
+    @Override
+      public void onFinish() {
+         OnTurnEnd();
+      }
+    
+    @Override
+      public void onTick(long millisUntilFinished) 
+      {
+     TextView countdownTxt = (TextView) findViewById( R.id.Timer );
+     countdownTxt.setText( ":" + Long.toString(( millisUntilFinished / 1000 ) + 1 ));
+      }
+  }; // End TurnTimer
+  
+  /**
+   * @return The animation that brings cards into view from the right of the
+   * screen
+   */
   private Animation InFromRightAnimation ()
   {
 	Animation inFromRight = new TranslateAnimation(
@@ -109,6 +178,10 @@ public class Turn extends Activity
   	return inFromRight;
   }
 
+  /**
+   * @return The animation that tosses the cards from the view out into the
+   * ether at the left of the screen
+   */
   private Animation OutToLeftAnimation ()
   {
 	Animation outToLeft = new TranslateAnimation(
@@ -124,8 +197,8 @@ public class Turn extends Activity
    */
   protected void ShowCard()
   {
-    int curTitle = 0;
-    int curWords = 0;
+    int curTitle;
+    int curWords;
     if( this.AIsActive )
     {
     	curTitle = R.id.CardTitleA;
@@ -152,57 +225,10 @@ public class Turn extends Activity
     cardWords.setAdapter( cardAdapter );
   }
   
-  private OnClickListener CorrectListener = new OnClickListener() 
-  {
-      public void onClick(View v) 
-      {
-        AIsActive = !AIsActive;
-        ViewFlipper flipper = (ViewFlipper) findViewById( R.id.ViewFlipper0 );
-        flipper.showNext();
-        curGameManager.ProcessCard( 0 );
-        ShowCard();
-      }
-  };
   
-  private OnClickListener SkipListener = new OnClickListener() 
-  {
-      public void onClick(View v) 
-      {
-        AIsActive = !AIsActive;
-        ViewFlipper flipper = (ViewFlipper) findViewById( R.id.ViewFlipper0 );
-        flipper.showNext();
-        curGameManager.ProcessCard( 2 );
-        ShowCard();
-      }
-  };
- 
-  /**
-   * CountdownTimer - This initializes a timer during every turn that runs a
-   * method when it completes as well as during update intervals.
-  */ 
-  public class TurnTimer extends CountDownTimer
-  {
-	  
-	  public TurnTimer(long millisInFuture, long countDownInterval) 
-	  {
-		  super(millisInFuture, countDownInterval);
-	  }
-	  
-	  @Override
-      public void onFinish() {
-         OnTurnEnd();
-      }
-	  
-	  @Override
-      public void onTick(long millisUntilFinished) 
-      {
-		 TextView countdownTxt = (TextView) findViewById( R.id.Timer );
-		 countdownTxt.setText( ":" + Long.toString(( millisUntilFinished / 1000 ) + 1 ));
-      }
-  };
 
   /**
-   * OnTurnEnd - Hands off the intent to the next turn summary activity.
+   * Hands off the intent to the next turn summary activity.
    */
   protected void OnTurnEnd( )
   {
