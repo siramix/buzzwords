df0da32697b53645a0344136544532c3e703f1df
Patrick Reynolds <cpreynolds@gmail.com>
2010-05-27 13:45:22 -0400

    Added comments to TaboozleApplication.

--- src/com/taboozle/TaboozleApplication.java
+++ src/com/taboozle/TaboozleApplication.java
@@ -1,33 +1,39 @@
-/**
- * 
- */
 package com.taboozle;
 
 import android.app.Application;
 
-
 /**
- * @author cpatrick
- *
+ * @author The Taboozle Team
+ * Class extending the standard android application. This allows us to refer
+ * to one GameManager from every activity within taboozle.
  */
 public class TaboozleApplication extends Application
 {
 
+  /**
+   * The GameManager for all of taboozle
+   */
   private GameManager gameManager;
   
   /**
-   * 
+   * Default constructor
    */
   public TaboozleApplication()
   {
     super();
   }
   
+  /**
+   * @return a reference to the game manager
+   */
   public GameManager GetGameManager()
   {
     return this.gameManager;
   }
   
+  /**
+   * @param gm - a reference to the game manager
+   */
   public void SetGameManager( GameManager gm )
   {
     this.gameManager = gm;
