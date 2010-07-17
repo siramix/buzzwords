03d4edb4d9ca206272f91ce6a58595d6e396d733
Patrick Reynolds <cpreynolds@gmail.com>
2010-07-17 15:46:12 -0400

    Removing warnings.

--- src/com/taboozle/GameEnd.java
+++ src/com/taboozle/GameEnd.java
@@ -1,37 +1,34 @@
 package com.taboozle;
 
-import java.util.ArrayList;
-
 import android.app.Activity;
 import android.os.Bundle;
-import android.widget.ListView;
-import android.widget.SimpleAdapter;
 import android.widget.TextView;
 
 /**
  * The GameEnd class is the final screen of the application, called
  * when either the number of turns is up, the time is up, the end game
  * button is clicked, or any other number of ways to end a game.
- * 
+ *
  * @author Taboozle team
  *
  */
 public class GameEnd extends Activity
 {
-      
-    public void onCreate( Bundle savedInstanceState ) 
+
+    @Override
+    public void onCreate( Bundle savedInstanceState )
     {
 		super.onCreate(savedInstanceState);
 		this.setContentView( R.layout.gameend );
-		  
-		ListView list = (ListView) findViewById(R.id.EndGameTurnList);
-		
-		TaboozleApplication application = 
+
+		//ListView list = (ListView) findViewById(R.id.EndGameTurnList);
+
+		TaboozleApplication application =
 			(TaboozleApplication) this.getApplication();
-		GameManager game = application.GetGameManager();      
-		
+		GameManager game = application.GetGameManager();
+
 		//ArrayList<Integer> roundScoreList = new ArrayList<Integer>();
-		
+
 		//for(int i=0; i<game.GetNumTeams(); ++i)
 		//{
 		//	round
@@ -39,18 +36,18 @@ public class GameEnd extends Activity
 		/*
 		SimpleAdapter roundScores = new SimpleAdapter(this, roundScoreList, R.layout.gameend,
 		            new String[] {"title", "rws"}, new int[] {R.id.TurnSum_CardTitle, R.id.TurnSum_CardRWS});
-		list.setAdapter(roundScores);	  
+		list.setAdapter(roundScores);
 	  */
-		long[] totalscores = game.GetTeamScores().clone();	
-		
+		long[] totalscores = game.GetTeamScores().clone();
+
 		TextView teamATotalScore = (TextView) findViewById(R.id.EndGameTeamAScore);
 		teamATotalScore.setText("Team A: " + Long.toString(totalscores[0]));
-		
+
 		TextView teamBTotalScore = (TextView) findViewById(R.id.EndGameTeamBScore);
 		teamBTotalScore.setText("Team B: " + Long.toString(totalscores[1]));
-		
+
 		TextView curTeam = (TextView) findViewById(R.id.EndGameCurTeamIndex);
-			curTeam.setText("Current Team: " + Long.toString(game.GetActiveTeamIndex())); 	  
-	  
+			curTeam.setText("Current Team: " + Long.toString(game.GetActiveTeamIndex()));
+
     }
 }
