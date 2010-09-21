03c4b157994a6648f6e61c5d9eef2a6466358a20
Patrick Reynolds <cpreynolds@gmail.com>
2010-09-21 00:57:31 -0400

    Added the ability to change scores in summary.
    I added a support function in card and a clever set of linked lists and an
    onClickListener that enables the clicking of the right/wrong/skip labels in the
    turn summary to change the scores from the previous turn before they are
    official and placed into the database.

--- src/com/taboozle/Card.java
+++ src/com/taboozle/Card.java
@@ -7,7 +7,7 @@ import android.util.Log;
 
 /**
  * @author The Taboozle Team
- * 
+ *
  * The Card class is a simple container class for storing the
  *
  */
@@ -17,27 +17,27 @@ public class Card
    * Static string used to refer to this class, in debug output for example.
    */
   private static final String TAG = "Card";
-  
+
   /**
    * The id of the card in the database
    */
   private long id;
-  
+
   /**
    * The right,wrong,skip {0,1,2} state of the card
    */
   private int rws;
-  
+
   /**
    * The title of the card, the word to be guessed
    */
   private String title;
-  
+
   /**
    * An array list of the words you cannot say
    */
   private ArrayList<String> badWords;
-  
+
   /**
    * Function for breaking a string into an array list of strings based on the
    * presence of commas. The bad words are stored in the database as a comma
@@ -55,19 +55,19 @@ public class Card
     {
       ret.add( tok.nextToken( "," ).toUpperCase() );
     }
-    
+
     return ret;
   }
-  
+
   /**
-   * Default constructor 
+   * Default constructor
    */
   public Card()
-  {    
+  {
     Log.d( TAG, "Card()" );
     this.init( -1, -1, "", new ArrayList<String>() );
   }
-  
+
   /**
    * Copy Constructor
    */
@@ -77,11 +77,11 @@ public class Card
     ArrayList<String> bws = new ArrayList<String>( rhs.getBadWords() );
     this.init( rhs.getId(), this.getRws(), this.getTitle(), bws );
   }
-  
+
   /**
    * Function for initializing card state
    */
-  private void init( long id, int rws, String title, 
+  private void init( long id, int rws, String title,
                      ArrayList<String> badWords )
   {
     Log.d( TAG, "init()" );
@@ -90,62 +90,62 @@ public class Card
     this.title = title;
     this.badWords = badWords;
   }
-  
+
   public long getId()
   {
     Log.d( TAG, "getId()" );
     return this.id;
   }
 
-  
+
   public void setId( long id )
   {
     Log.d( TAG, "setId()" );
     this.id = id;
   }
 
-  
+
   public int getRws()
-  {    
+  {
     Log.d( TAG, "getRws()" );
     return this.rws;
   }
 
-  
+
   public void setRws( int rws )
   {
     Log.d( TAG, "setRws()" );
     this.rws = rws;
   }
 
-  
+
   public String getTitle()
   {
     Log.d( TAG, "getTitle()" );
     return this.title;
   }
 
-  
+
   public void setTitle( String title )
   {
     Log.d( TAG, "setTitle()" );
     this.title = title;
   }
 
-  
+
   public ArrayList<String> getBadWords()
   {
     Log.d( TAG, "getBadWords()" );
     return this.badWords;
   }
 
-  
+
   public void setBadWords( ArrayList<String> badWords )
   {
     Log.d( TAG, "setBadWords(ArrayList<String>)" );
     this.badWords = badWords;
   }
-  
+
   /**
    * Override setter for a comma-separated string
    * @param commaSeparated
@@ -153,9 +153,9 @@ public class Card
   public void setBadWords( String commaSeparated )
   {
     Log.d( TAG, "setBadWords(String)" );
-    this.badWords = Card.BustString( commaSeparated ); 
+    this.badWords = Card.BustString( commaSeparated );
   }
-  
+
   /**
    * Get the resource ID for this card's right wrong skip icon
    */
@@ -172,6 +172,19 @@ public class Card
   	default:
   	  return R.drawable.skip;
   	}
+  }
+
+  /**
+   * Cycle right/wrong/skip for the turn summary
+   */
+  public void cycleRws()
+  {
+    Log.d( TAG, "cycleRws()" );
+    this.rws++;
+    if( rws == 3 )
+    {
+      rws = 0;
+    }
   }
 
 }
--- src/com/taboozle/TurnSummary.java
+++ src/com/taboozle/TurnSummary.java
@@ -28,7 +28,10 @@ public class TurnSummary extends Activity
    * logging tag
    */
   public static String TAG = "TurnSummary";
-  
+
+  private LinkedList<Card> cardList;
+  private LinkedList<ImageView> cardViewList;
+
 	/**
 	  * Watches the button that handles hand-off to the next turn activity.
 	  */
@@ -36,7 +39,7 @@ public class TurnSummary extends Activity
 	  {
 	      public void onClick(View v)
 	      {
-	        Log.d( TAG, "NextTurnListener OnClick()" );         
+	        Log.d( TAG, "NextTurnListener OnClick()" );
 	        TaboozleApplication application =
 	          (TaboozleApplication) TurnSummary.this.getApplication();
 	        GameManager gm = application.GetGameManager();
@@ -45,6 +48,20 @@ public class TurnSummary extends Activity
 	      }
 	  }; // End NextTurnListener
 
+	  private final OnClickListener CardIconListener = new OnClickListener()
+    {
+        public void onClick(View v)
+        {
+          ImageView iv = (ImageView) v;
+          int cardIndex = TurnSummary.this.cardViewList.indexOf( v );
+          Card curCard = TurnSummary.this.cardList.get( cardIndex );
+          curCard.cycleRws();
+          Log.d( TAG, Integer.toString( cardIndex ) );
+          iv.setImageResource( curCard.getDrawableId() );
+          TurnSummary.this.UpdateScoreViews();
+        }
+    };
+
 	  /**
 	   * Watches the end turn button that cancels the game.
 	   */
@@ -52,32 +69,32 @@ public class TurnSummary extends Activity
 	  {
 	      public void onClick(View v)
 	      {
-          Log.d( TAG, "EndGameListener OnClick()" );         	        
+          Log.d( TAG, "EndGameListener OnClick()" );
 	        AlertDialog confirmEnd = new AlertDialog.Builder(v.getContext()).create();
 	        confirmEnd.setTitle("Confirm End Game");
 	        confirmEnd.setMessage("Are you sure you want to end the current game?");
-	        
-	        confirmEnd.setButton("Cancel", new DialogInterface.OnClickListener() 
+
+	        confirmEnd.setButton("Cancel", new DialogInterface.OnClickListener()
           {
-            public void onClick(DialogInterface dialog, int which) {                           
+            public void onClick(DialogInterface dialog, int which) {
             }
           });
-	        
-	        confirmEnd.setButton2("Yes", new DialogInterface.OnClickListener() 
+
+	        confirmEnd.setButton2("Yes", new DialogInterface.OnClickListener()
 	        {
             public void onClick(DialogInterface dialog, int which) {
               TaboozleApplication application =
                 (TaboozleApplication) TurnSummary.this.getApplication();
               GameManager gm = application.GetGameManager();
               gm.EndGame();
-              startActivity(new Intent(Intent.ACTION_CALL, getIntent().getData()));              
+              startActivity(new Intent(Intent.ACTION_CALL, getIntent().getData()));
             }
           });
-         
+
 	        confirmEnd.show();
 	      }
 	  }; // End OnClickListener
-	  
+
   /**
   * onCreate - initializes the activity to display the results of the turn.
   */
@@ -85,80 +102,83 @@ public class TurnSummary extends Activity
   public void onCreate( Bundle savedInstanceState )
   {
   	super.onCreate( savedInstanceState );
-    Log.d( TAG, "onCreate()" );  
+    Log.d( TAG, "onCreate()" );
   	// Setup the view
   	this.setContentView(R.layout.turnsummary);
-  
+
   	ScrollView list = (ScrollView) findViewById(R.id.TurnSumCardList);
   	LinearLayout layout = new LinearLayout(this.getBaseContext());
   	layout.setOrientation(LinearLayout.VERTICAL);
       TaboozleApplication application =
           (TaboozleApplication) this.getApplication();
       GameManager game = application.GetGameManager();
-  
-  	LinkedList<Card> cardlist = game.GetCurrentCards();
+
+    this.cardViewList = new LinkedList<ImageView>();
+  	this.cardList = game.GetCurrentCards();
   	Card card = null;
   	int count = 0;
-  	for( Iterator<Card> it = cardlist.iterator(); it.hasNext(); )
+  	for( Iterator<Card> it = this.cardList.iterator(); it.hasNext(); )
   	{
   	  card = it.next();
-  		
+
   	  LinearLayout line = (LinearLayout) LinearLayout.inflate(this.getBaseContext(), R.layout.turnsumrow, layout);
   	  LinearLayout realLine = (LinearLayout) line.getChildAt(count);
-  	  
+
   	  TextView cardTitle = (TextView) realLine.getChildAt(0);
   	  cardTitle.setText(card.getTitle());
-  	  
+
   	  ImageView cardIcon = (ImageView) realLine.getChildAt(1);
+  	  this.cardViewList.addLast( cardIcon );
   	  cardIcon.setImageResource(card.getDrawableId());
+  	  cardIcon.setOnClickListener( CardIconListener );
   	  count++;
   	}
   	list.addView(layout);
-  	
+
   	UpdateScoreViews();
-  
+
   	Button playGameButton = (Button)this.findViewById( R.id.TurnSumNextTurn );
   	playGameButton.setOnClickListener( NextTurnListener );
-  
+
   	Button endGameButton = (Button)this.findViewById( R.id.TurnSumEndGame );
   	endGameButton.setOnClickListener( EndGameListener );
   }
-  
+
   /**
    * Update the views to display the proper scores for the current round
    */
   private void UpdateScoreViews()
   {
-    Log.d( TAG, "UpdateScoreViews()" );  
+    Log.d( TAG, "UpdateScoreViews()" );
     TaboozleApplication application =
           (TaboozleApplication) this.getApplication();
     GameManager game = application.GetGameManager();
-  
+
   	long turnscore = game.GetTurnScore();
   	long[] totalscores = game.GetTeamScores().clone();
-  
+
   	totalscores[game.GetActiveTeamIndex()] += turnscore;
-  
+
   	TextView scoreview = (TextView) findViewById(R.id.TurnTotalScore);
   	scoreview.setText(Long.toString(turnscore));
-  
+
   	int[] scoreViewIds = new int[]{R.id.TeamAScore, R.id.TeamBScore};
   	for (int i = 0; i < scoreViewIds.length; i++)
   	{
   		TextView teamTotalScoreView = (TextView) findViewById(scoreViewIds[i]);
   		teamTotalScoreView.setText(game.GetTeamNames()[i] + ": " + Long.toString(totalscores[i]));
   	}
-  
+
       TextView curTeam = (TextView) findViewById(R.id.CurTeamIndex);
       curTeam.setText("Current Team: " + Long.toString(game.GetActiveTeamIndex()));
   }
-  
+
   /**
    * Handler for key down events
    */
   @Override
   public boolean onKeyDown(int keyCode, KeyEvent event)
-  {  
+  {
     // Handle the back button
     if( keyCode == KeyEvent.KEYCODE_BACK
         && event.getRepeatCount() == 0 )
@@ -166,10 +186,10 @@ public class TurnSummary extends Activity
         event.startTracking();
         return true;
       }
-  
+
     return super.onKeyDown(keyCode, event);
    }
-  
+
   /**
    * Handler for key up events
    */
@@ -182,7 +202,7 @@ public class TurnSummary extends Activity
       {
       return true;
       }
-  
+
     return super.onKeyUp(keyCode, event);
   }
 
