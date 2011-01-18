222d86c7552da3d11616b03f5797a7545579a967
Lucas Rowe <lucas.rowe@gmail.com>
2011-01-17 22:20:07 -0500

    Negligible changes made to only test class.  I'll need some help on this Patrick.

--- tests/src/com/taboozle/test/GameTest.java
+++ tests/src/com/taboozle/test/GameTest.java
@@ -2,10 +2,19 @@ package com.taboozle.test;
 
 import android.test.ActivityTestCase;
 
-public class GameTest extends ActivityTestCase {
-
+/**
+ * Test case for the Game database definition class.  This should be able 
+ * to run asserts that various data sets match our expectations.
+ * 
+ * @author Lucas
+ *
+ */
+public class GameTest extends ActivityTestCase 
+{
+  
   public GameTest()
   {
+    super();
   }
   
   protected void setUp() throws Exception {
