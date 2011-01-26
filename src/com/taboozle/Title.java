c1a29a1f44022770a71beff3af575f15615826b6
Edward Rowe <tehgiver@gmail.com>
2011-01-26 01:10:36 -0500

    Rearranged TitleScreen to fit new Concept, v4.
    Reworked all button pngs and added labels to handle 45 degree
    slant.  Added background image (currently the wrong size).
    Added animation to bring in all elements independently.
    Fixed a typo in Turn.java.

--- src/com/taboozle/Title.java
+++ src/com/taboozle/Title.java
@@ -5,8 +5,13 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.ViewGroup;
 import android.view.View.OnClickListener;
 import android.widget.ImageButton;
+import android.view.animation.Animation;
+import android.view.animation.LinearInterpolator;
+import android.view.animation.RotateAnimation;
+import android.view.animation.TranslateAnimation;
 
 /**
  * This is the activity class that kicks off Taboozle
@@ -62,7 +67,40 @@ public class Title extends Activity
       
     }
   };
-	
+
+  /**
+   * @return The animation that orients the button list 45
+   * screen
+   */
+  private Animation TranslateButtons(int buttonNum)
+  {
+    
+    Log.d( TAG, "RotateButtons()" );
+    TranslateAnimation slideIn = new TranslateAnimation(
+        Animation.RELATIVE_TO_PARENT,  (-1.0f * buttonNum), Animation.RELATIVE_TO_PARENT,  0.0f,
+        Animation.RELATIVE_TO_PARENT,  (0.7f * buttonNum), Animation.RELATIVE_TO_PARENT,   0.0f );
+    slideIn.setDuration(600 + ( 200 * buttonNum) );
+    slideIn.setInterpolator(new LinearInterpolator());
+    return slideIn;
+  }
+
+  /**
+   * @return The animation that orients the button list 45
+   * screen
+   */
+  private Animation TranslateLabels(int labelNum)
+  {
+    
+    Log.d( TAG, "RotateButtons()" );
+    TranslateAnimation slideIn = new TranslateAnimation(
+        Animation.RELATIVE_TO_PARENT,  (1.0f * labelNum), Animation.RELATIVE_TO_PARENT,  0.0f,
+        Animation.RELATIVE_TO_PARENT,  (0.7f * labelNum), Animation.RELATIVE_TO_PARENT,   0.0f );
+    slideIn.setDuration(800 + ( 200 * ( labelNum + 1 ) ) );
+    slideIn.setInterpolator(new LinearInterpolator());
+    return slideIn;
+  }
+  
+  
 /**
 * onCreate - initializes a welcome screen that starts the game.
 */
@@ -83,6 +121,24 @@ public void onCreate( Bundle savedInstanceState )
   
   ImageButton rulesButton = (ImageButton) this.findViewById( R.id.Title_RulesButton );
   rulesButton.setOnClickListener( RulesListener );
+  
+  View button = (View) this.findViewById( R.id.Title_PlayButton);
+  button.startAnimation(this.TranslateButtons(1));
+  button = (View) this.findViewById( R.id.Title_BuzzButton);
+  button.startAnimation(this.TranslateButtons(2));
+  button = (View) this.findViewById( R.id.Title_SettingsButton);
+  button.startAnimation(this.TranslateButtons(3));
+  button = (View) this.findViewById( R.id.Title_RulesButton);
+  button.startAnimation(this.TranslateButtons(4));
+  
+  View label = (View) this.findViewById( R.id.Title_PlayButton_Label);
+  label.startAnimation(this.TranslateLabels(1));
+  label = (View) this.findViewById( R.id.Title_BuzzButton_Label);
+  label.startAnimation(this.TranslateLabels(2));
+  label = (View) this.findViewById( R.id.Title_SettingsButton_Label);
+  label.startAnimation(this.TranslateLabels(3));
+  label = (View) this.findViewById( R.id.Title_RulesButton_Label);
+  label.startAnimation(this.TranslateLabels(4));
 }
 
 }
