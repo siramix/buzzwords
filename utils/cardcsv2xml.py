99f2e3cf464e233f11bb9df48091e2fd3807ea0f
Patrick Reynolds <cpreynolds@gmail.com>
2011-04-08 01:54:00 -0400

    Adding utility scripts for making world packs.

---
+++ utils/cardcsv2xml.py
@@ -0,0 +1,51 @@
+from xml.dom.minidom import getDOMImplementation
+import sys
+import re
+
+if len(sys.argv) != 3:
+  print "Usage: %s <csv input> <xml output>" % sys.argv[0]
+  sys.exit()
+
+f = open(sys.argv[1],'r')
+
+impl = getDOMImplementation()
+
+newdoc = impl.createDocument(None, 'pack', None)
+top = newdoc.documentElement
+
+for line in f.readlines():
+  row = line.split(',')
+
+  cardEl = newdoc.createElement('card')
+
+  titleEl = newdoc.createElement('title')
+  titleTxt = newdoc.createTextNode(row[0])
+  titleEl.appendChild(titleTxt)
+
+  badWordsEl = newdoc.createElement('bad-words')
+
+  packNameEl = newdoc.createElement('pack-name')
+  packNameTxt = newdoc.createTextNode(row[7])
+  packNameEl.appendChild(packNameTxt)
+
+  catEl = newdoc.createElement('categories')
+  catTxt = newdoc.createTextNode(row[6])
+  catEl.appendChild(catTxt)
+
+  for i in range(1,6):
+    wordEl = newdoc.createElement('word')
+    wordTxt = newdoc.createTextNode(row[i].upper())
+    wordEl.appendChild(wordTxt)
+    badWordsEl.appendChild(wordEl)
+  cardEl.appendChild(packNameEl)
+  cardEl.appendChild(titleEl)
+  cardEl.appendChild(catEl)
+  cardEl.appendChild(badWordsEl)
+  top.appendChild(cardEl)
+
+outfile = open(sys.argv[2], 'w')
+uglyXml = newdoc.toprettyxml(indent='  ')
+text_re = re.compile('>\n\s+([^<>\s].*?)\n\s+</', re.DOTALL)    
+prettyXml = text_re.sub('>\g<1></', uglyXml)
+outfile.write(prettyXml)
+outfile.close()
---
+++ utils/cardxml2csv.py
@@ -0,0 +1,21 @@
+from xml.dom.minidom import parse
+import sys
+
+if len(sys.argv) != 3:
+  print "Usage: %s <xml input> <csv output>" % sys.argv[0]
+  sys.exit()
+dom = parse(sys.argv[1])
+f = open(sys.argv[2],'w')
+cards = dom.getElementsByTagName('card')
+
+for card in cards:
+  f.write(card.getElementsByTagName('title')[0].childNodes[0].data + ',')
+  words = card.getElementsByTagName('bad-words')[0].getElementsByTagName('word')
+  for word in words:
+    f.write(word.childNodes[0].data.upper() + ',')
+  f.write(card.getElementsByTagName('categories')[0].childNodes[0].data + ',')
+  f.write(card.getElementsByTagName('pack-name')[0].childNodes[0].data + ',')
+  f.write('\n')
+
+f.close()
+  
