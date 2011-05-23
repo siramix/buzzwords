8ea7ca6e141ae3012ceebd92b28f0a50e4299f9f
Edward Rowe <tehgiver@gmail.com>
2011-05-22 22:46:57 -0400

    Added program description line to copyright disclaiqer.

--- src/com/buzzwords/SplashScreen.java
+++ src/com/buzzwords/SplashScreen.java
@@ -1,4 +1,7 @@
 /*****************************************************************************
+ *  Buzzwords is a family friendly word game for mobile phones.
+ *  Copyright (C) 2011 Siramix Team
+ *  
  *  This program is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
  *  the Free Software Foundation, either version 3 of the License, or
