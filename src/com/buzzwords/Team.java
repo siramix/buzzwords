13421c444f2b89d041e350f2342c837a636a6baa
Edward Rowe <tehgiver@gmail.com>
2011-05-25 01:46:32 -0400

    Cleaned up XML for turn and removed unused color.
    Card views (for title and badwords) now use styles for all their
    attributes.

--- src/com/buzzwords/Team.java
+++ src/com/buzzwords/Team.java
@@ -1,6 +1,20 @@
-/**
- * 
- */
+/*****************************************************************************
+ *  Buzzwords is a family friendly word game for mobile phones.
+ *  Copyright (C) 2011 Siramix Team
+ *  
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ ****************************************************************************/
 package com.buzzwords;
 
 import java.util.Comparator;
@@ -9,28 +23,30 @@ import com.buzzwords.R;
 
 
 /**
- * @author Siramix Labs
+ * Team is a data representation of a Team in buzzwords.  It mainly consists of
+ * getters and setters, and contains references to various team specific attributes
+ * like colors.
+ * 
+ * @author BuzzWords team
  *
  */
 public enum Team
 {
-  TEAMA( "Blue", R.color.teamA_BG, R.color.teamA_CardText, 
+  TEAMA( "Blue", R.color.teamA_BG,  
          R.color.teamA_secondary, R.color.teamA_text, R.drawable.bg_bluegradient,
          R.drawable.gameend_row_end_blue),
-  TEAMB( "Green", R.color.teamB_BG, R.color.teamB_CardText, 
+  TEAMB( "Green", R.color.teamB_BG, 
          R.color.teamB_secondary, R.color.teamB_text, R.drawable.bg_greengradient,
          R.drawable.gameend_row_end_green),
-  TEAMC( "Red", R.color.teamC_BG, R.color.teamC_CardText, 
+  TEAMC( "Red", R.color.teamC_BG,  
          R.color.teamC_secondary, R.color.teamC_text, R.drawable.bg_redgradient,
          R.drawable.gameend_row_end_red),
-  TEAMD( "Yellow", R.color.teamD_BG, R.color.teamD_CardText, 
-         R.color.teamD_secondary, R.color.teamD_text, R.drawable.bg_yellowgradient,
+  TEAMD( "Yellow", R.color.teamD_BG,     R.color.teamD_secondary, R.color.teamD_text, R.drawable.bg_yellowgradient,
          R.drawable.gameend_row_end_yellow);
   
   
   private final String name;
   private final int bg;
-  private final int cardText;
   private final int secondary;
   private final int text;
   private final int gradient;
@@ -39,11 +55,10 @@ public enum Team
 
   private int score;
   
-  private Team( String name, int bg, int cardText, int secondary, int text, int gradient, int gameend_bg)
+  private Team( String name, int bg, int secondary, int text, int gradient, int gameend_bg)
   {
     this.name = name;
     this.bg = bg;
-    this.cardText = cardText;
     this.secondary = secondary;
     this.text = text;
     this.gradient = gradient;
@@ -68,16 +83,6 @@ public enum Team
   {
     return bg;
   }
-
-  
-  /**
-   * @return the cardText color
-   */
-  public int getCardText()
-  {
-    return cardText;
-  }
-
   
   /**
    * @return the secondary color
