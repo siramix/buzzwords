6939949ea967b16cc3944d11a9ca252dc3331fa4
Patrick Reynolds <cpreynolds@gmail.com>
2011-05-26 21:57:01 -0400

    Updated XML file to final.  Unflagged Debug.

--- src/com/buzzwordslite/BuzzWordsApplication.java
+++ src/com/buzzwordslite/BuzzWordsApplication.java
@@ -32,7 +32,7 @@ public class BuzzWordsApplication extends Application {
   /**
    * Global Debug constant
    */
-  public static final boolean DEBUG = true;
+  public static final boolean DEBUG = false;
 
   /**
    * logging tag
