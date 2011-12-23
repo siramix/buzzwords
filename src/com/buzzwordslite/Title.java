fad026dd677805f4a747c76f153b2178506a8c92
LucasRowe <lucas.rowe@gmail.com>
2011-12-23 02:53:21 -0500

    Adding holiday dialog in progress.

--- src/com/buzzwordslite/Title.java
+++ src/com/buzzwordslite/Title.java
@@ -19,10 +19,12 @@ package com.buzzwordslite;
 
 
 import com.buzzwordslite.R;
+import com.buzzwordslite.BuzzWordsApplication.Markets;
 
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.Dialog;
+import android.app.AlertDialog.Builder;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
@@ -33,6 +35,7 @@ import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.text.Html;
 import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
@@ -73,7 +76,17 @@ public class Title extends Activity {
    * Dialog constant for second Rate Us message
    */
   static final int DIALOG_RATEUS_SECOND = 1;
-    
+  
+  /**
+   * Dialog constant for second Rate Us message
+   */
+  static final int DIALOG_HOLIDAY_GIVEAWAY = 2;
+  
+  /**
+   * TEMPORARY variable for holiday giveaway key
+   */
+  private final String giveaway_prefkey = "show_giveaway"; 
+  
   /**
    * PlayGameListener is used for the start game button. It launches the next
    * activity.
@@ -358,9 +371,14 @@ public class Title extends Activity {
     // Capture our play count to decide whether to show the Rate Us dialog
     int playCount = sp.getInt(getResources().getString(R.string.PREFKEY_PLAYCOUNT), 0);
     boolean showReminder = sp.getBoolean(getResources().getString(R.string.PREFKEY_SHOWREMINDER), false);
+    boolean showGiveAway = sp.getBoolean(giveaway_prefkey, true);
+    showGiveAway = true;
+    if (showGiveAway) {
+      showDialog(DIALOG_HOLIDAY_GIVEAWAY);
+    }
     
     // If 3 plays have been done and reminder is not muted, show dialog
-    if (showReminder) {
+    if (showReminder && !showGiveAway) {
       if (playCount < 6) {
         showDialog(DIALOG_RATEUS_FIRST);
       }
@@ -516,6 +534,17 @@ public class Title extends Activity {
   }
   
   /**
+   * TEMPORARY METHOD for muting the holiday gift dialog
+   */
+  private void muteGiftDialog() {
+    BuzzWordsApplication application = (BuzzWordsApplication) getApplication();    
+    SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(application.getBaseContext());
+    SharedPreferences.Editor prefEditor = sp.edit();   
+    
+    prefEditor.putBoolean(giveaway_prefkey, false);                  
+    prefEditor.commit();
+  }
+  /**
    * Custom create Dialogs
    */
   @Override
@@ -582,7 +611,40 @@ public class Title extends Activity {
             });
       dialog = builder.create();
       break;
-      
+    
+    case DIALOG_HOLIDAY_GIVEAWAY:
+        builder = new AlertDialog.Builder(this);
+        builder
+          .setTitle("A Holiday Gift from Siramix!")
+          .setMessage("To celebrate the holidays Buzzwords will be entirely free until the new year!  " + 
+                      "We hope that you enjoy playing with your friends and family over the holidays.")
+          .setPositiveButton("Upgrade (free)", 
+              new DialogInterface.OnClickListener() {            
+                public void onClick(DialogInterface dialog, int id) {
+                  //TODO FIX THIS
+                  Intent intent = new Intent(Intent.ACTION_VIEW, BuzzWordsApplication.storeURI);
+                  startActivity(intent);
+                  muteGiftDialog();
+                }
+                 
+          }).setNeutralButton("Share with friends", 
+              new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int id) {
+                  Intent sharingIntent = new Intent(Intent.ACTION_SEND);
+                  sharingIntent.setType("text/plain");
+                  sharingIntent.putExtra(android.content.Intent.EXTRA_TEXT, "EXTRA TEXT HERE");
+                  startActivity(Intent.createChooser(sharingIntent,"Share using"));                  
+                  muteGiftDialog();
+                }
+          }).setNegativeButton("No Thanks", 
+              new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int which) {
+                  muteGiftDialog();
+                }
+          });
+      dialog = builder.create();
+      break;
+                      
     default:
       dialog = null;
     }
