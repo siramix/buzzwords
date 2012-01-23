cb7e0b1b3270a3e4c9dcd3674f7b37bc9f053fb6
Patrick Reynolds <cpreynolds@gmail.com>
2012-01-22 22:44:08 -0500

    Fixing cancelled state of dialogs. Relevant to lite only.

--- src/com/buzzwordslite/GameEnd.java
+++ src/com/buzzwordslite/GameEnd.java
@@ -238,6 +238,7 @@ public class GameEnd extends Activity {
       // Play confirm sound
       SoundManager sm = SoundManager.getInstance(GameEnd.this.getBaseContext());
       sm.playSound(SoundManager.Sound.CONFIRM);
+      v.setEnabled(true);
 
     }
   }; // End MainMenuListener
@@ -257,6 +258,7 @@ public class GameEnd extends Activity {
       // Play confirm sound
       SoundManager sm = SoundManager.getInstance(GameEnd.this.getBaseContext());
       sm.playSound(SoundManager.Sound.CONFIRM);
+      v.setEnabled(true);
     }
   }; // End MainMenuListener
 
@@ -430,11 +432,8 @@ public class GameEnd extends Activity {
       builder = new AlertDialog.Builder(this);
       builder
           .setMessage(
-              //"Want the full BuzzWords experience with a full deck of 1000 Words? Buy the full version today!")
-              "The full version of Buzzwords is a reduced price until the new year!  But hey, " + 
-              "if you'd rather keep playing the lite version, we won't stop you.")
-          //.setPositiveButton("Yeah!", new DialogInterface.OnClickListener() {
-          .setPositiveButton("Upgrade!", new DialogInterface.OnClickListener() {
+              "Want the full BuzzWords experience with a full deck of 1000 Words? Buy the full version today!")
+          .setPositiveButton("Yeah!", new DialogInterface.OnClickListener() {
             public void onClick(DialogInterface dialog, int id) {              
               Intent intent = new Intent(Intent.ACTION_VIEW, BuzzWordsApplication.storeURI_Buzzwords);              
               startActivity(intent);
