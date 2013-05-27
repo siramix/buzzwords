7ab2fbb23e68235ff16d35797066e1da64ae40f1
LucasRowe <lucasrowepublic@gmail.com>
2013-05-27 16:40:44 -0400

    Added templated Config that leverages ant to filter in properties.

--- config/Config.java
+++ config/Config.java
@@ -7,12 +7,11 @@ package com.buzzwords;
  */
 public class Config {
 
-  public static final boolean DEBUG_TIMERTICKS = false;
-  public static String packBaseUri = @CONFIG.PACKURI@;
-  public static String storeURI_Buzzwords = @CONFIG.BUZZWORDSSTORE@;
-  public static String storeURI_BuzzwordsLite = @CONFIG.BUSSWORDSLITESTORE@;
+  public static final String packList = "packs.json";
+  public static final String packBaseUri = @CONFIG.PACKURI@;
+  public static final String storeUriBuzzwords = @CONFIG.BUZZWORDSSTORE@;
+  public static final String storeUriBuzzwordsLite = @CONFIG.BUSSWORDSLITESTORE@;
   
-  public static final String buzzwordsRedirectUri = "http://www.siramix.com/buzzwordsappstore";
   public static final String buzzwordsFBAppLauncher = "fb://page/472759256084535";
   public static final String buzzwordsFBPage = "https://www.facebook.com/buzzwordsapp";
 }
--- src/com/buzzwords/Config.java
+++ src/com/buzzwords/Config.java
@@ -1,21 +1,17 @@
 package com.buzzwords;
 
+/**
+ * Configurable constants belong in this file. This file serves as a template
+ * that can be modified at build time by ant by parameterizing any const. Ant
+ * will copy this file to src/ and make any necessary replacements.
+ */
 public class Config {
-
- // public static final boolean DEBUG_TIMERTICKS = false;
-  public static final String storeUriBuzzwords = "https://market.android.com/details?id=com.buzzwords";
-  public static final String storeUriBuzzwordsLite = ">https://market.android.com/details?id=com.buzzwordslite";
-
-  public static final String buzzwordsRedirectUri = "http://www.siramix.com/buzzwordsappstore";
-  public static final String buzzwordsFBAppLauncher = "fb://page/472759256084535";
-  public static final String buzzwordsFBPage = "https://www.facebook.com/buzzwordsapp";
 
   public static final String packList = "packs.json";
   public static final String packBaseUri = "https://s3.amazonaws.com/siramix.buzzwords/bw-packdata-test/";
-/*  <string name="URI_buzzwords_redirect">http://www.siramix.com/buzzwordsappstore</string>
-  <string name="URI_android_market_buzzwords">https://market.android.com/details?id=com.buzzwords</string>
-  <string name="URI_android_market_buzzwordslite">https://market.android.com/details?id=com.buzzwordslite</string>
-  <string name="URI_amazon_market_buzzwords">http://www.amazon.com/gp/mas/dl/android?p=com.buzzwords</string>
-  <string name="URI_amazon_market_buzzwordslite">http://www.amazon.com/gp/mas/dl/android?p=com.buzzwordslite</string>
-*/
+  public static final String storeUriBuzzwords = "http://www.amazon.com/gp/mas/dl/android?p=com.buzzwords";
+  public static final String storeUriBuzzwordsLite = "http://www.amazon.com/gp/mas/dl/android?p=com.buzzwordslite";
+  
+  public static final String buzzwordsFBAppLauncher = "fb://page/472759256084535";
+  public static final String buzzwordsFBPage = "https://www.facebook.com/buzzwordsapp";
 }
