d2e53801ceaa1faa57bc4fc75affb76c30d3197e
Edward Rowe <tehgiver@gmail.com>
2013-06-26 23:47:53 -0400

    Added Tap text and clickability to TutorialLayout.

--- src/com/buzzwords/TutorialLayout.java
+++ src/com/buzzwords/TutorialLayout.java
@@ -28,8 +28,8 @@ import android.view.animation.TranslateAnimation;
 import android.widget.RelativeLayout;
 
 /**
- * Custom view that pairs a mask with a chat bubble and handles showing,
- * hiding, and advancing tutorial text.
+ * Custom view that pairs a mask with a chat bubble and handles showing, hiding,
+ * and advancing tutorial text.
  * 
  * @author Siramix Labs
  * 
@@ -41,15 +41,19 @@ public class TutorialLayout extends RelativeLayout {
    */
   private MaskView mMask;
   private ChatBubbleLayout mChat;
-  
+
   private View mCurrentTarget;
   private String mCurrentText;
   private int mCurrentChatLocation;
 
+  private FrancoisOneTextView mTapText;
+
   public final static int BOTTOM = 0;
   public final static int CENTER = 1;
   public final static int TOP = 2;
-  
+
+  private OnClickListener mClickListener;
+
   /**
    * @param context
    */
@@ -83,53 +87,84 @@ public class TutorialLayout extends RelativeLayout {
    */
   private void init(Context context) {
 
-    // Initialize the mask
     mMask = new MaskView(context);
-    RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(
-      LayoutParams.FILL_PARENT, LayoutParams.FILL_PARENT);
-    mMask.setLayoutParams(params);
-  
-    // Initialize the chat bubble
     mChat = new ChatBubbleLayout(context);
-    RelativeLayout.LayoutParams chatParams = new RelativeLayout.LayoutParams(
-        LayoutParams.FILL_PARENT, LayoutParams.WRAP_CONTENT);
-    mChat.setPadding(0, 10, 0, 10);
-    mChat.setLayoutParams(chatParams);
-  
+    mTapText = new FrancoisOneTextView(context);
     // Assign IDs to our elements
     int id = 0;
     mMask.setId(++id);
     mChat.setId(++id);
+    mTapText.setId(++id);
+
+    // Initialize the mask
+    RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(
+        LayoutParams.FILL_PARENT, LayoutParams.FILL_PARENT);
+    mMask.setLayoutParams(params);
+
+    // Initialize the chat bubble
+    RelativeLayout.LayoutParams chatParams = new RelativeLayout.LayoutParams(
+        LayoutParams.FILL_PARENT, LayoutParams.WRAP_CONTENT);
+    mChat.setPadding(0, 10, 0, 10);
+    mChat.setLayoutParams(chatParams);
+
+    mTapText.setText("* Tap *");
+    RelativeLayout.LayoutParams tapParams = new RelativeLayout.LayoutParams(
+        LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
+    tapParams.addRule(RelativeLayout.ALIGN_BOTTOM, mChat.getId());
+    tapParams.bottomMargin = 20;
+    tapParams.leftMargin = 30;
+    mTapText.setTextSize(22);
+    mTapText.setLayoutParams(tapParams);
+    mTapText.setVisibility(View.INVISIBLE);
 
     // Add the views to the layout
     this.addView(mMask);
     this.addView(mChat);
+    this.addView(mTapText);
 
-    // Hide the view, to be shown programatically by Activities that use this Layout
-    this.setVisibility(View.INVISIBLE);
+    // Initialize the view unclickable to allow click events underneath it
+    this.setClickable(false);
+
+    // Hide the view, to be shown programatically by Activities that use this
+    // Layout
+    this.setVisibility(View.GONE);
   }
-  
+
   /**
-   * Show the tutorial overlay, highlighting the specified view and displaying the text as spoken
-   * by Buzz.
-   * @param View target - The view to highlight using the MaskView
-   * @param String text - The text for Buzz to speak
-   * @param int chatLocation - the location for the chat bubble, as defined by TutorialLayout
+   * Show the tutorial overlay
    */
-  public void show(View target, String text, int chatLocation) {
-    
+  private void show() {
+
+    // Set the view clickable. This blocks all clickable events on views the
+    // layout covers
+    this.setClickable(true);
+
+    // Show the view so that it returns from GONE
     this.setVisibility(View.VISIBLE);
-    
+
+    this.setEnabled(false);
+
+    mChat.setVisibility(View.INVISIBLE);
+    mMask.setAnimation(fadeInMaskAnimation());
+  }
+
+  /**
+   * Set the content of the tutorial, highlighting the specified view and
+   * displaying the text as spoken by Buzz.
+   * 
+   * @param View
+   *          target - The view to highlight using the MaskView
+   * @param String
+   *          text - The text for Buzz to speak
+   * @param int chatLocation - the location for the chat bubble, as defined by
+   *        TutorialLayout
+   */
+  public void setContent(View target, String text, int chatLocation) {
+    // Setup the data fields
     mCurrentTarget = target;
     mCurrentText = text;
     mCurrentChatLocation = chatLocation;
 
-    mMask.setAnimation(fadeInAnimation());
-    
-    mChat.setVisibility(View.INVISIBLE);
-    
-    //mChat.setText(mCurrentText);
-    
     // Put the chat box in the desired location
     RelativeLayout.LayoutParams chatParams = (RelativeLayout.LayoutParams) mChat
         .getLayoutParams();
@@ -138,30 +173,82 @@ public class TutorialLayout extends RelativeLayout {
     case BOTTOM:
       rule = RelativeLayout.ALIGN_PARENT_BOTTOM;
       break;
-
     case CENTER:
       rule = RelativeLayout.CENTER_IN_PARENT;
       break;
-
     case TOP:
       rule = RelativeLayout.ALIGN_PARENT_TOP;
       break;
     }
     chatParams.addRule(rule);
+
+    // Show the view if it's hidden, or else just refresh the views to represent
+    // the data
+    if (this.getVisibility() == View.GONE) {
+      show();
+    } else {
+      refreshViews();
+    }
   }
-  
+
+  /**
+   * Set the content of the tutorial, highlighting the specified view and
+   * displaying the text as spoken by Buzz.
+   * 
+   * @param text
+   * @param chatLocation
+   */
+  public void setContent(String text, int chatLocation) {
+    setContent(null, text, chatLocation);
+  }
+
+  /**
+   * Hides the tutorial page and all its children via animation
+   */
+  public void hide() {
+    // Have Buzz stop speaking
+    mChat.hideChat();
+    // Slide out Buzz
+    mChat.setAnimation(slideOutChatAnimation());
+    // Fade out the mask
+    mMask.setAnimation(fadeOutMaskAnimation());
+    // Fade the tap text out
+    mTapText.setAnimation(fadeOutTapAnimation());
+
+    // Allow them to click views under the tutorial while fading
+    this.setClickable(false);
+  }
+
+  /**
+   * Sets a listener to get callbacks when the tutorial is clicked.
+   */
+  public void setListener(OnClickListener listener) {
+    mClickListener = listener;
+    this.setOnClickListener(mClickListener);
+  }
+
+  /**
+   * Refresh the content in the views represented by the TutorialLayout. This
+   * causes the chat bubble to pop up if it adds text, and sets the target area.
+   */
+  private void refreshViews() {
+    mChat.setText(mCurrentText);
+    mMask.setTarget(mCurrentTarget);
+
+    // Show *Tap* text over some time.
+    mTapText.setAnimation(fadeInTapAnimation());
+  }
+
   /*
    * Returns an animation to play when shown
    */
-  private AlphaAnimation fadeInAnimation()
-  {
-    AlphaAnimation fadeIn = new AlphaAnimation(0,1);
+  private AlphaAnimation fadeInMaskAnimation() {
+    AlphaAnimation fadeIn = new AlphaAnimation(0, 1);
     fadeIn.setDuration(1000);
     fadeIn.setFillAfter(true);
-    AnimationListener listener = new AnimationListener() 
-    {
+    AnimationListener listener = new AnimationListener() {
       public void onAnimationEnd(Animation animation) {
-        mChat.setAnimation(slideInChatAnimation());        
+        mChat.setAnimation(slideInChatAnimation());
         mChat.setVisibility(View.VISIBLE);
       }
 
@@ -174,21 +261,78 @@ public class TutorialLayout extends RelativeLayout {
     fadeIn.setAnimationListener(listener);
     return fadeIn;
   }
-  
+
+  /*
+   * Returns the animation that fades out the Mask
+   */
+  private AlphaAnimation fadeOutMaskAnimation() {
+    AlphaAnimation fadeOut = new AlphaAnimation(1, 0);
+    fadeOut.setDuration(1000);
+    fadeOut.setFillAfter(true);
+    AnimationListener listener = new AnimationListener() {
+      public void onAnimationEnd(Animation animation) {
+        // Hide the whole layout after fading
+        TutorialLayout.this.setVisibility(View.GONE);
+      }
+
+      public void onAnimationRepeat(Animation animation) {
+      }
+
+      public void onAnimationStart(Animation animation) {
+      }
+    };
+    fadeOut.setAnimationListener(listener);
+    return fadeOut;
+  }
+
+  /*
+   * Returns the animation that fades in the Tap Text, enabling click events
+   */
+  private AlphaAnimation fadeInTapAnimation() {
+    AlphaAnimation fadeIn = new AlphaAnimation(0, 1);
+    fadeIn.setStartOffset(800);
+    fadeIn.setDuration(200);
+    fadeIn.setFillAfter(true);
+    AnimationListener listener = new AnimationListener() {
+      public void onAnimationEnd(Animation animation) {
+        // Tap text is faded in, allow clicks
+        TutorialLayout.this.setEnabled(true);
+      }
+
+      public void onAnimationRepeat(Animation animation) {
+      }
+
+      public void onAnimationStart(Animation animation) {
+        // While fading in the Tap text, don't let them click the view
+        TutorialLayout.this.setEnabled(false);
+      }
+    };
+    fadeIn.setAnimationListener(listener);
+    return fadeIn;
+  }
+
+  /*
+   * Returns the animation that fades out the Tap text
+   */
+  private AlphaAnimation fadeOutTapAnimation() {
+    AlphaAnimation fadeOut = new AlphaAnimation(1, 0);
+    fadeOut.setDuration(100);
+    fadeOut.setFillAfter(true);
+    return fadeOut;
+  }
+
   /*
    * Returns the slide in animation for the chat element
    */
-  private TranslateAnimation slideInChatAnimation()
-  {
+  private TranslateAnimation slideInChatAnimation() {
     TranslateAnimation slideIn = new TranslateAnimation(0, 0, 1200, 0);
     slideIn.setFillBefore(true);
     slideIn.setDuration(600);
     slideIn.setInterpolator(new OvershootInterpolator(0.6f));
-    AnimationListener listener = new AnimationListener() 
-    {
+    AnimationListener listener = new AnimationListener() {
       public void onAnimationEnd(Animation animation) {
-        mChat.setText(mCurrentText);
-        mMask.setTarget(mCurrentTarget);
+        // Refresh the content after character has slid in
+        refreshViews();
       }
 
       public void onAnimationRepeat(Animation animation) {
@@ -199,6 +343,18 @@ public class TutorialLayout extends RelativeLayout {
     };
     slideIn.setAnimationListener(listener);
     return slideIn;
+  }
+
+  /*
+   * Returns the slide out animation for the chat element
+   */
+  private TranslateAnimation slideOutChatAnimation() {
+    TranslateAnimation slideOut = new TranslateAnimation(0, 0, 0, 1200);
+    slideOut.setFillAfter(true);
+    // This offset is a bit hacky. It's waiting for the chat to hide.
+    slideOut.setStartOffset(300);
+    slideOut.setDuration(600);
+    return slideOut;
   }
 
 }
